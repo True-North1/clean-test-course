e873863486c3ded3f16559f86d5dd9c83995b64d
True-North1 <evan.tal7@gmail.com>
2025-11-20 14:50:09 +0100

    Add Cypress acceptance_test job and enable full Lesson 6 pipeline

--- frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
+++ frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
@@ -3,9 +3,17 @@ describe('Shows Appeteasers', () => {
     cy.visit('https://hangryhippo.quantic.host/');
   });
   it('selects Handhelds', () => {
-    //TODO: Add a test to check Handhelds
+    // Find the category item that has the text "Handhelds" and click it
+    cy.contains('Handhelds').click();
+    cy.contains('Cheese Burger');
+    cy.contains('Fajita Tacos');
   });
   it('selects Appeteasers', () => {
-    //TODO: Add a test to check Appeteasers
+    // Find the category item that has the text "Appeteasers" and click it
+    cy.contains('Appeteasers').click();
+    cy.contains('Tater Tots');
+    cy.contains('Buffalo Wings');
+    cy.contains('Cheese Burger').should('not.exist');
+    cy.contains('Fajita Tacos').should('not.exist');
   });
 });
